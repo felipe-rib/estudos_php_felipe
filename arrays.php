957f5ff2905890733bc401cbe65cbff1a81c8477
felipe-rib <felipeasr3@gmail.com>
2024-05-15 09:03:57 -0300

    Novo

---
+++ arrays.php
@@ -0,0 +1,9 @@
+<?php
+
+$carros = [
+    1 => 'Up',
+    2 =>'Gol',
+    3 =>'Mobi'
+];
+
+var_dump(is_array($carros));
---
+++ notas.php
@@ -0,0 +1,13 @@
+<?php
+
+$notas = [
+    'Felipe' => null,
+    'Clara' => 8,
+    'Rodrigo' => 7
+];
+
+
+
+if (isset($notas['Felipe']) == 0) {
+    echo "Não foi a prova";
+}
